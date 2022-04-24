81c5681583142856467c317b2aa1acde98a502ec
wineT <tolya.bogomolov2019@gmail.com>
2022-04-24 11:06:02 +1000

    added more views (without templates)

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/hello')
+def hello_page():
+    return render_template('hello.html')

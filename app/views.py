664601ab5d79f6321915c7d1bffd9b68e6cb84a7
wineT <tolya.bogomolov2019@gmail.com>
2022-04-24 11:46:27 +1000

    added more templates

--- app/views.py
+++ app/views.py
@@ -5,16 +5,16 @@ from flask import render_template, redirect
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Панель управления')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title='Вход')
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('register.html', title='Регистрация')
 
 @app.route('/hello')
 def hello_page():
-    return render_template('hello.html')
+    return render_template('hello.html', title='Тевирп!')

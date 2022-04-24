69bb166870456851ee004a6cbc36b1954ab2be60
wineT <tolya.bogomolov2019@gmail.com>
2022-04-24 19:11:40 +1000

    flask_migrate lib

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
 
-from app import views
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import views, models
 
 from app.api import error_handlers
 from app.api import hive_handlers
